7a31b64e8ab2bd3cc2c6b924bc500ac36b069d70
Thecodercomau <roahansenthilkumar0034257@gmail.com>
2026-04-22 17:54:41 +0800

    Main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-number = int(input("Enter a Number for validation: "))
-print("Checking the number :", number)
+height = float(input("Enter your height in CM: "))
+weight = float(input("Enter your weight in KG: "))
 
-if number%2==0 :
-    print("The number is Even")
+BMI = weight / (height / 100) ** 2 
+print("Your BMI is: ", BMI)
+
+if BMI <= 18.4:
+    print("You are underweight.")
+elif BMI <= 24.9:
+    print("You are healthy and perfect.")
+elif BMI <= 29.9:
+    print("You are overweight.")
+elif BMI <= 34.9:
+    print("You are severely overweight.")
+elif BMI <= 39.9:
+    print("You are obese. Warning!")
 else:
-    print("The Number is Odd")
+    print("You are severely obese. Warning! Warning!")
