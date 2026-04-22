04cc1354db568c9d0854f0b30100170d050fc80d
Thecodercomau <roahansenthilkumar0034257@gmail.com>
2026-04-22 17:45:17 +0800

    Main

---
+++ main.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a Number for validation: "))
+print("Checking the number :", number)
+
+if number%2==0 :
+    print("The number is Even")
+else:
+    print("The Number is Odd")
